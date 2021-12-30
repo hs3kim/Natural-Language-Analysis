16b37f4da03615127b402685488d31f763143622
Han Su <hs3kim@uwaterloo.ca>
2021-12-29 23:29:37 -0500

    -Init class to store stock instances -Update user agent header for requests to be random

--- stock_news_scraper.py
+++ stock_news_scraper.py
@@ -1,20 +1,55 @@
 from bs4 import BeautifulSoup
 import requests
 import datetime as dt
+from fake_useragent import UserAgent
 
-# Scrapes news articule urls from the input domain published within the past 12 hours
+ua = UserAgent()
+
+class Stock:
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.news_titles = []
+        self.total_score = 0
+        self.total_magnitude = 0
+    
+    def append_news(self, news):
+        self.news_titles.append(news)
+
+    def get_score(self):
+        if len(self.news_titles) > 0:
+            return self.total_score/len(self.news_titles)
+        else:
+            return 0
+
+    def get_magnitude(self):
+        if len(self.news_titles) > 0:
+            return self.total_magnitude/len(self.news_titles)
+        else:
+            return 0
+
+# Initiate Stock objects into a dictionary
+def create_list(file_name):
+    stock_dict = {}
+    with open(file_name, "r") as f:
+        for ticker in f:
+            stock_dict[ticker.strip()] = Stock(ticker.strip())
+    return stock_dict
+
+
+# Scrapes titles of news articles for a stock
 def scrape_urls(stock):
-    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+    HEADER={'User-Agent': ua.chrome}
 
     url_finviz = "https://finviz.com/quote.ashx?t={}"
-    req = requests.get(url_finviz.format(stock), headers=headers)
+    req = requests.get(url_finviz.format(stock), headers=HEADER)
     doc = BeautifulSoup(req.text, "html.parser")
-    
+
     news_table = doc.find(id='news-table').find_all("tr")
 
-    # obtains all news urls 
+    # obtain all time values
+    news_date = None
     for idx, news in enumerate(news_table):
-        news_table[idx] = news.find("a")["href"]
+        news_table[idx] = (news.find("td")).string
         
     return news_table
 
