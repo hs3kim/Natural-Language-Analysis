809bf857bf17565854667a90e0c5e2c8186d5eb6
Han Su <hs3kim@uwaterloo.ca>
2021-12-31 02:42:29 -0500

    General comments added

--- NL_Analysis.py
+++ NL_Analysis.py
@@ -1,5 +1,6 @@
 from google.cloud import language_v1
 from dotenv import load_dotenv
+from stock_news_scraper import *
 import os
 
 load_dotenv()
@@ -16,12 +17,15 @@ def analyze_text(text):
     # Detects the sentiment of the text
     sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
 
+    # Score: Overall emotion (-1.0 ~ 1.0)
+    # Magnitude: Strength of emotion (0.0 ~ +inf)
     return sentiment.score, sentiment.magnitude
 
 def main():
-    sample_text = "BEIJING (Reuters) - Tesla Inc's Shanghai factory is expected to produce 300,000 cars in the first nine months of the year, capped by a delivery rush in the end of the July-September quarter, despite a global semiconductor shortage, two sources said. The factory makes the electric Model 3 sedans and Model Y sport-utility vehicles for domestic and international markets, including Germany and Japan. Around 240,000 vehicles were shipped from the factory in the first eight months, including many for export, according to data from the China Passenger Car Association. Tesla has not announced details on the factory's production. The sources requested anonymity, as they were not allowed to speak to media. Tesla did not immediately respond to a request for comment. In August, an official in the area where Tesla's factory is located said it is expected to product 450,000 vehicles this year, including 66,100 for export. Tesla is hiring managers for legal and external relations teams in China as it faces public scrutiny in the country over data security and customer service complaints."
+    sample_text = "Tesla is recalling almost as many vehicles as it delivered last yea"
     score, magnitude = analyze_text(sample_text)
     print("score: {}, magnitude: {}".format(score, magnitude))
+    
 
 if __name__=="__main__":
     main()
