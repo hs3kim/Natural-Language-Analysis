fcb52b4a2fe40a1c568d1f372c72cb1f6aa739b8
Han Su <hs3kim@uwaterloo.ca>
2021-12-31 02:41:49 -0500

    Init Stock objects with ticker & scraped news

--- stock_news_scraper.py
+++ stock_news_scraper.py
@@ -1,19 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
-import datetime as dt
+from datetime import datetime, timedelta
 from fake_useragent import UserAgent
+import unicodedata
 
 ua = UserAgent()
+LOCAL_DT = datetime.now()
+SCRAPE_NUM_HR = 24
 
 class Stock:
-    def __init__(self, ticker):
+    def __init__(self, ticker, news_titles):
         self.ticker = ticker
-        self.news_titles = []
+        self.news_titles = news_titles
         self.total_score = 0
         self.total_magnitude = 0
-    
-    def append_news(self, news):
-        self.news_titles.append(news)
 
     def get_score(self):
         if len(self.news_titles) > 0:
@@ -32,32 +32,46 @@ def create_list(file_name):
     stock_dict = {}
     with open(file_name, "r") as f:
         for ticker in f:
-            stock_dict[ticker.strip()] = Stock(ticker.strip())
+            news_titles = scrape_news(ticker.strip(), SCRAPE_NUM_HR)
+            stock_dict[ticker.strip()] = Stock(ticker.strip(), news_titles)
     return stock_dict
 
 
 # Scrapes titles of news articles for a stock
-def scrape_urls(stock):
+def scrape_news(stock, num_hr):
     HEADER={'User-Agent': ua.chrome}
 
     url_finviz = "https://finviz.com/quote.ashx?t={}"
     req = requests.get(url_finviz.format(stock), headers=HEADER)
     doc = BeautifulSoup(req.text, "html.parser")
-
     news_table = doc.find(id='news-table').find_all("tr")
 
-    # obtain all time values
+    # Format datetime string
     news_date = None
-    for idx, news in enumerate(news_table):
-        news_table[idx] = (news.find("td")).string
+    news_lst = []
+    for news in news_table:
+        unparsed_dt = news.find("td").string
+        # Date & time given in unparsed datetime
+        if len(unparsed_dt) == 19:
+            news_date = (unparsed_dt)[:9]
+            news_dt_str = unparsed_dt[:17]
+        # Only time given in in unparsed datetime
+        else:
+            news_dt_str = news_date + " " + unparsed_dt[:7]
+
+        news_dt_obj = datetime.strptime(news_dt_str, '%b-%d-%y %I:%M%p')
         
-    return news_table
+        # Add to list if news were published within the past num_hr
+        if ((LOCAL_DT - timedelta(hours=num_hr)) < news_dt_obj):
+            news_lst.append(news.find("a").string)
+        else:
+            break
+
+    return news_lst
 
 def main():
-    stock_tickers = ["FB", "AMZN", "AAPL", "NFLX", "GOOG"]
-    url_dict = {}
-    for ticker in stock_tickers:
-        url_dict[ticker] = scrape_urls(ticker)
+    ticker_dict = create_list("./s&p500_ticker.txt")
+    # print(ticker_dict['AAPL'].news_titles)
 
 if __name__=="__main__":
     main()
